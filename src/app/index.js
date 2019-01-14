c3f120e79cef1d1cba16146469496d6e700478ac
Dass987 <sequeradanniel@gmail.com>
2019-01-14 17:09:31 -0400

    github clone v0.1

---
+++ src/app/index.js
@@ -0,0 +1,6 @@
+const ui = require('./ui');
+const Github = require('./github');
+const { client_id, client_secret } = require('./config.json');
+const github = new Github(client_id, client_secret);
+
+document.getElementById("");
